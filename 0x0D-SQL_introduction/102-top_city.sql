5b70e473580c6db925833871d08c7cd325f9213b
Mohamed Ayman <mmohamedaayman1315125@gmail.com>
2023-11-14 22:48:42 +0200

    SQL - Introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,6 @@
--- Imports temperatures.sql into hbtn_0c_0-- displays top 3 cities temp during Aug and July in desc
-SELECT `city`, AVG(`value`) AS avg_temp FROM `temperatures`
-WHERE `month`=7 or `month`= 8 
-GROUP BY city ORDER BY avg_temp
+--  displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+
+SELECT city ,AVG(value) as avg_temp FROM temperatures
+WHERE (month = 7 or month = 8)
+GROUP BY city ORDER BY avg_temp DESC
 LIMIT 3;
