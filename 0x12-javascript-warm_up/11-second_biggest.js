3b76868998f0cb06bc01e1afea120ac3964fbf14
Mohamed Ayman <mmohamedaayman1315125@gmail.com>
2023-12-11 14:47:05 +0200

    JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
-const scndBig = process.argv
-  .slice(2)
-  .map(Number)
-  .sort((a, b) => b - a)[1];
-console.log(scndBig === undefined ? 0 : scndBig);
+
+const args = process.argv;
+const values = process.argv.slice(2);
+if (args.length < 4) {
+  console.log('0');
+} else {
+  for (const x in values) {
+    values[x] = parseInt(values[x]);
+  }
+  values.sort(function (a, b) {
+    return a - b;
+  });
+  values.reverse();
+  console.log(values[1]);
+}
