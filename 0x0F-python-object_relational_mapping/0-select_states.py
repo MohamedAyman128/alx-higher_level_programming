f94701701705b6bddf416d25d3d2f375f3fa5ee8
Mohamed Ayman <mmohamedaayman1315125@gmail.com>
2023-12-23 02:06:35 +0200

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
+'''
+Lists all states from the database hbtn_0e_0_usa.
+
+Usage:
+    ./0-select_states.py <username> <password> <database>
+
+Parameters:
+    - <username>: MySQL database username.
+    - <password>: MySQL database password.
+    - <database>: Name of the MySQL database.
+
+Note: No argument validation needed.
+
+Example:
+    ./0-select_states.py root root hbtn_0e_0_usa
+'''
+
 import sys
+import MySQLdb
 
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        mydb = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM states ORDER BY id ASC;")
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor_db = db.cursor()
-    cursor_db.execute("SELECT * FROM states")
-    rows = cursor_db.fetchall()
-    for row in rows:
-        print(row)
-    cursor_db.close()
-    db.close()
+        rows = mycursor.fetchall()
+
+        for row in rows:
+            print(row)
+        mydb.close()
